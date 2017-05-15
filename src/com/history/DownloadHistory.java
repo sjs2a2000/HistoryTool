065856c47680bcd3bd466d212285564b3e66b177
scott <scottandsophia@gmail.com>
2017-05-15 07:11:05 -0400

    updated history to include indexes/etf

--- src/com/history/DownloadHistory.java
+++ src/com/history/DownloadHistory.java
@@ -24,7 +24,7 @@ public class DownloadHistory {
     HashMap<String, String> periodMap = new HashMap<>();
     String[] windows = new String[]{"d", "w", "m"};
     String[] exchanges = new String[]{"nyse", "amex", "nasdaq"};
-    String[] othersources = new String[]{"nasdaqlisted", "otherlisted"};
+    String[] othersources = new String[]{"nasdaqlisted.txt", "otherlisted.txt", "IndexSymbols.csv"};
 
     {
         fileMap.put("d", "c:/prices/daily/");
@@ -111,9 +111,8 @@ public class DownloadHistory {
 
     void DownloadSymbols() throws IOException {
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        //make the directory to store data if it does not exist
         System.out.println("DownloadSymbols");
-;        File dir = new File("c:/prices/stocksdb"); //this throws error if it does not exist??
+        File dir = new File("c:/prices/stocksdb"); //this throws error if it does not exist??
         System.out.println("test dir");
         if(dir.exists()) {
             System.out.println("dir exits for writing history");
@@ -138,6 +137,22 @@ public class DownloadHistory {
         }catch(Exception err){
             System.out.println("download of ftp failed" + "\nERROR:" + err.toString());
         }
+        try {
+            File indexes = new File("./data/IndexSymbols.csv");
+            System.out.println("orig path = " + indexes.getAbsolutePath());
+            File indexesnew = new File("c:/prices/stocksdb/IndexSymbols.csv");
+            System.out.println("new path = " + indexesnew.getAbsolutePath());
+            if( indexesnew.exists()) {
+                System.out.println("index file already exists in stocksdb: " + indexesnew.getAbsolutePath() + " skipping copy");
+            }else if(indexes.exists()) {
+                System.out.println("index file exists, need to copy");
+                FileUtils.copyFile(indexes, indexesnew);
+            }else{
+                System.out.println("no index file exists, skipping copy orig:"  + indexes.getAbsolutePath() + " , new:" + indexesnew.getAbsolutePath());
+            }
+        }catch(Exception err){
+            System.out.println("Failed to copy file IndexSymbols.csv " + err.getMessage());
+        }
     }
 
     ArrayList<String> getSymbolList() throws FileNotFoundException {
@@ -145,7 +160,6 @@ public class DownloadHistory {
         for(String exch : exchanges ) {
             File f = new File(String.format("c:/prices/stocksdb/%s.txt",exch));
             Scanner scanner = new Scanner(f);
-            //scanner.useDelimiter(",");
             while (scanner.hasNextLine())
             {
                 String line = scanner.nextLine();
@@ -155,20 +169,23 @@ public class DownloadHistory {
             scanner.close();
             System.out.println(exch);
         }
-
         for(String other : othersources ) {
-            File f = new File(String.format("c:/prices/stocksdb/%s.txt",other));
+            File f = new File(String.format("c:/prices/stocksdb/%s",other));
             Scanner scanner = new Scanner(f);
-            //scanner.useDelimiter("|");
+            String[] fields;
+            if(other.contains("Index") && scanner.hasNextLine())
+                scanner.nextLine();
             while (scanner.hasNextLine())
             {
                 String line = scanner.nextLine();
-                String[] fields = line.split("|");
+                if(other.contains("Index"))
+                    fields = line.split(",");
+                else
+                    fields = line.split("|");
                 symbols.add(fields[0].replaceAll("\\s", ""));
             }
             scanner.close();
         }
-        //convert symbols from set to arraylist
         ArrayList<String> temp = new ArrayList<String>();
         temp.addAll(symbols);
         Collections.sort(temp);
@@ -176,30 +193,26 @@ public class DownloadHistory {
         return temp;
     }
 
-    void DownloadSymbolPrice(String symbol)
-    {   try {
+    void DownloadSymbolPrice(String symbol, HashMap<String, String> mapping)
+    {
+        try {
             for (String window : windows) {
                 File dir = new File(fileMap.get(window));
                 if (!dir.exists())
                     dir.mkdirs();
                 String period = periodMap.get(window);
                 symbol = symbol.replace("\"", "");
-                //if (symbol.equals("Symbol"))
-                //    continue;
-                //if(!symbol.equals("DCO"))
-                //    continue;
-
-                System.out.println("Next: " + symbol);
-                String path = fileMap.get(window) + symbol + ".txt";
+                String filesym = symbol;
+                if(mapping.containsKey(symbol))
+                    filesym = mapping.get(symbol);
+                String path = fileMap.get(window) + filesym + ".txt";
                 String linkSym = symbol;
-                if(symbol.contains("^"))
+                if(symbol.contains("^") && !mapping.containsKey(symbol))
                     linkSym = symbol.replace("^","-P");
-
-                if(symbol.contains("."))
+                if(symbol.contains(".") &&  !mapping.containsKey(symbol))
                 {
                     linkSym = linkSym.replaceFirst("\\.","-");
                     linkSym = linkSym.replaceFirst("\\.","");
-                    System.out.println("linksym="+linkSym);
                 }
                 String linkStr = String.format("https://chart.finance.yahoo.com/table.csv?s=%s&%s&g=%s&ignore=.csv", linkSym, period, window);
                 System.out.println("Symbol:"+symbol + " link: " + linkStr);
@@ -217,7 +230,22 @@ public class DownloadHistory {
 
     void DownloadPrices()
     {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+        //load the index file and create mapping of symbol to file
+        HashMap<String,String> mapping = new HashMap<String, String>();
+        try {
+            File f = new File(String.format("c:/prices/stocksdb/IndexSymbols.csv"));
+            Scanner scanner = new Scanner(f);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] fields = line.split(",");
+                mapping.put(fields[0], fields[2]);
+                //symbols.add(fields[0].replaceAll("\\s", ""));
+            }
+            scanner.close();
+        }catch(Exception err){
+            System.out.println("failed to load indexSymbols file");
+        }
+        //ExecutorService executor = Executors.newFixedThreadPool(5);
         try {
             System.out.println("DownloadPrices");
             int index = 0;
@@ -226,16 +254,16 @@ public class DownloadHistory {
                 //executor.execute(() -> {
                 //    DownloadSymbolPrice(symbol);
                 //});
-                DownloadSymbolPrice(symbol);
+                DownloadSymbolPrice(symbol, mapping);
                 ++index;
                 System.out.println("symbol:" + symbol + " done, symbol count:" + symbols.size() + " number complete:" + index);
             }
         }catch(Exception err) {
             System.out.println("download prices failed" + err.toString());
         }
-        executor.shutdown();
-        while (!executor.isTerminated()) {
-        }
+        //executor.shutdown();
+        //while (!executor.isTerminated()) {
+        //}
         System.out.println("Finished all threads");
     }
 }
